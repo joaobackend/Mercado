aeb22ee76bbb490d6f0e12fa2fb333390738b0ba
joao-pedro13134 <jpdeza11@gmail.com>
2025-11-14 14:17:04 -0300

    Correção

--- src/main/java/com/senai/backend/mercado/controllers/MercadoriaController.java
+++ src/main/java/com/senai/backend/mercado/controllers/MercadoriaController.java
@@ -4,11 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.senai.backend.mercado.models.Mercadoria;
 import com.senai.backend.mercado.services.MercadoriaService;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping
 
 public class MercadoriaController {
     
