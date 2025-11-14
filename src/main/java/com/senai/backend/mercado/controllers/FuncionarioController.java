981d2b3e3504ee7fd6433c7118d3c63f11f9db16
joao-pedro13134 <jpdeza11@gmail.com>
2025-11-14 14:12:52 -0300

    Controller

--- src/main/java/com/senai/backend/mercado/controllers/FuncionarioController.java
+++ src/main/java/com/senai/backend/mercado/controllers/FuncionarioController.java
@@ -1,5 +1,32 @@
 package com.senai.backend.mercado.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senai.backend.mercado.models.Funcionario;
+import com.senai.backend.mercado.services.FuncionarioService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @PostMapping("/cadastrar")
+    public Funcionario cadastrar(@RequestBody Funcionario funcionario){
+        return funcionarioService.cadastrar(funcionario);
+
+    }
     
+    @GetMapping("/recuperar-por-id/{id}")
+    public Funcionario recuperarPorId(@PathVariable Integer id){
+        return funcionarioService.recuperarPorId(id);
+    }
 }
--- src/main/java/com/senai/backend/mercado/controllers/MercadoriaController.java
+++ src/main/java/com/senai/backend/mercado/controllers/MercadoriaController.java
@@ -1,5 +1,28 @@
 package com.senai.backend.mercado.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.senai.backend.mercado.models.Mercadoria;
+import com.senai.backend.mercado.services.MercadoriaService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 public class MercadoriaController {
     
+    @Autowired
+    private MercadoriaService mercadoriaService;
+
+    @PostMapping("/cadastrar")
+    public Mercadoria cadastrar(@RequestBody Mercadoria mercadoria){
+        return mercadoriaService.cadastrar(mercadoria);
+
+    }
+    
+    @GetMapping("/recuperar-por-id/{id}")
+    public Mercadoria recuperarPorId(@PathVariable Integer id){
+        return mercadoriaService.recuperarPorId(id);
+    }
 }
