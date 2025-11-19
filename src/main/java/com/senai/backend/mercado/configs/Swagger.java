f3d4c78580e4979f7867f2f19fb1fcce2246e009
joao-pedro13134 <jpdeza11@gmail.com>
2025-11-19 16:34:50 -0300

    Nome swagger

--- src/main/java/com/senai/backend/mercado/configs/Swagger.java
+++ src/main/java/com/senai/backend/mercado/configs/Swagger.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @Configuration
 @OpenAPIDefinition(
     info = @Info(
-        title = "Giuseppe Mercadorias",
+        title = "A verdadeira Mercadoria são os amigos que fazemos pelo caminho",
         version = "1.0",
         description = "João Pedro de Souza Rodrigues."
     )
